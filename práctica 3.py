7e093096aa5afbaabf568d0c3611c0447b8a40d8
Joan <jdagasa@gmail.com>
2024-08-29 16:41:27 -0500

    listo

---
+++ práctica 3.py
@@ -0,0 +1,11 @@
+Lista = [4,54,66.77,32,11,34,99,73,65,10,40,101]
+
+cont = 0
+for elem in Lista:
+    cont += 1
+print(f"la lista tiene {cont} elementos")
+
+
+#comprobar
+num_elementos = len(Lista)
+print(f"usando la funcion len nos da {num_elementos}")
