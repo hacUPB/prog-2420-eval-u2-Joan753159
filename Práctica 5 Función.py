f2b4fdde056ada4c3bcf0f4b1eb426e3fbde882c
Joan <jdagasa@gmail.com>
2024-09-05 17:29:34 -0500

    practica de clase

---
+++ Práctica 5 Función.py
@@ -0,0 +1,10 @@
+#calcula cuantos datos están por encima del promedio en una lista y cuales son esos datos
+def sobre_promedio(lista:list, promedio:float):
+    resultado = 23
+    return resultado
+pass
+#++++++++++++++++++++++++++++++++++++++++++programa++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+lista = list(input("ingrese la lista: "))
+print (f"La lista ingresada es: {lista}")
+Promedio = float(input("ingrese el promedio: "))
+print (f"El promedio ingresado es: {Promedio}")
